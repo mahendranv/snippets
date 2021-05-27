b201033d4bb307c70c7a04c8b95b37aa83dd3d75
Mahendran Vadivalagan <mahendran.vadivalagan@gmail.com>
2021-05-27 15:45:52 +0530

    Longest Substring Without Repeating Characters

--- leetcode/L3SubString.kt
+++ leetcode/L3SubString.kt
@@ -1,11 +1,95 @@
 package leet
 
 class Solution {
-    fun lengthOfLongestSubstring(s: String): Int {
+
+    /** 
+     * 
+     * SlidingWindow
+     * 
+     *                                                       i
+     * a     b     c     d      a     b    c     d     e     f
+     *                          |
+     *                         *start----------*j
+     * 
+     */
+    fun lengthOfLongestSubstringSlidingWindow(s: String): Int {
+        var start = 0
+        var length = 0
+        var max = 0
         
+        outer@for ((i, c) in s.withIndex()) {
+            length = 1
+            inner@for (j in start until i) {
+                if (s[j] == c) {
+                    start = j+1
+                    break@inner
+                } else {
+                    length++
+                }
+            }
+            max = Math.max(length, max)
+        }
+        return max
+    }
+
+
+    /**
+     * 
+     * Sliding window with memory.
+     * 
+     * |  a   |  4  |
+     * |  b   |  5  |
+     * ...
+     * 
+     * a     b     c     d      a     b    c     d     e     f
+     *                          |
+     *                         *start---------length-------------*i
+     * 
+     */
+    fun lengthOfLongestSubstring(s: String): Int {
+        var start = 0
+        var max = 0
+        var occ = java.util.HashMap<Char, Int>()
+        
+        // println("|#|start|end|char|subsequence|length|")
+        // println("|--| --- | --- | --- | --- | --- |")
+        for ((i, c) in s.withIndex()) {
+            var clash = ""
+            if (occ.containsKey(c)) {
+                val seekPoint = occ[c]!! + 1
+                start = Math.max(seekPoint, start)
+                clash = "*"
+            }
+            val length = i - start + 1
+            // println("|$i|$start|$i|$c$clash|${s.subSequence(start, i+1)}|$length|")
+            max = Math.max(length, max)
+
+            // Update the map with latest occurance
+            // when clash happen next time, use this index
+            occ[c] = i
+        }
+        println("Max($s) = $max")
+        return max
     }
 }
 
 fun main(args: Array<String>) {
-    println("Hello world")
+
+    val solution = Solution()
+
+    val set = listOf(
+        "",
+        "a",
+        "aa",
+        "ab",
+        "abca",
+        "abcdefghijk",
+        "abccabcde",
+        "abccabdefghabcababcab",
+        "abcamnijabxyzzz",
+    )
+
+    for (s in set) {
+        solution.lengthOfLongestSubstring(s)
+    }
 }
